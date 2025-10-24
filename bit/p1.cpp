be0a72a4309ba9ead0543304def0c86e5fc5f97c
Devesh Shrivas <114324384+Deveshshrivas@users.noreply.github.com>
2025-10-24 16:29:15 +0000

    added

---
+++ bit/p1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // int a = 0;
+    // int b = 2;
+    // cout << (a & b)<<endl;
+    // cout << (a|b)<<endl;
+    // cout << (~a)<<endl;
+    // cout << (a ^ b)<<endl;
+    // cout<<(17>>1)<<endl;
+    // cout<<(17>>2)<<endl;
+    // cout<<(19<<1)<<endl;
+    // cout<<(19<<2)<<endl;
+    // int i = 7;
+    //  cout<<++i<<endl;
+    // cout<<i++<<endl;
+    //  cout<<i--<<endl;
+    //   cout<<--i<<endl;
+  // if (a-- >0 || ++b >2){
+  //   cout<<"Stage1 - Inside If";
+  // }
+  // else{
+  //   cout<<"stage2 - Inside else";
+  // }
+  // cout<<a<<" "<<b<<endl;
+  // int a = 1;
+  // int b = a++;
+  // int c = ++a;
+  // cout<<b;
+  // cout<<c;
+
+    return 0;
+} 
--- pattern14.cpp
+++ pattern14.cpp
@@ -3,16 +3,16 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    int i=1;
+    int i =1;
     while(i<=n){
         int j =1;
-        
+        char ch = 'A'+n-i;
         while(j<=i){
-            char ch = 'A'+i+j-2;
-            cout<<ch<< " ";
+            cout<<ch<<" ";
+            ch = ch+1;
             j= j+1;
-        } 
+        }
         cout<<endl;
-        i =i+1;
+        i = i+1;
     }
 }
